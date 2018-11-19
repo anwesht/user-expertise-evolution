614b1d57015a4c96f843bb53c2dbbd84a6549769
anwesht <anwesh.tuladhar@gmail.com>
2018-11-19 15:52:12 -0500

    Cleaned up.

--- tocsv.py
+++ tocsv.py
@@ -1,3 +1,7 @@
+# Author: Anwesh Tuladhar <anwesh.tuladhar@gmail.com>
+# Website: https://anwesht.github.io/
+__author__ = 'Anwesh Tuladhar'
+
 
 def tocsv(filename, output):
     with open(filename, 'r') as inp, open(output, 'w') as out:
@@ -17,28 +21,3 @@ def tocsv(filename, output):
 
 if __name__ == "__main__":
     tocsv("data/reviews_for_each_user.txt", "data/reviews_for_each_user.csv")
-
-
-"""
-  def __create_dataset(self, filename):
-        cur_user_id = 0
-        cur_movie_id = 0
-
-        with open(filename, 'r') as f:
-            for line in f:
-                movieId, userId, rating, timestamp = line.split(',')
-
-                if userId in self.userToId:
-                    self.userToId[userId] = cur_user_id
-                    self.idToUser[cur_user_id] = userId
-                    cur_user_id += 1
-
-                if movieId in self.movieToId:
-                    self.movieToId[movieId] = cur_movie_id
-                    self.idToMovie[cur_movie_id] = movieId
-                    cur_movie_id += 1
-
-                cur_data = DataRow(self.userToId[userId], self.movieToId[movieId], float(rating), int(timestamp))
-
-                self.dataset.append(cur_data)
-"""
